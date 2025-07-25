3594686766a99c9c9ccdb2ea572521cb4b345138
saMujbo <samuelbarrantes1@gmail.com>
2025-07-24 23:36:49 -0600

    Crate-roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
       synchronize: true, // ❗️Solo para desarrollo (crea tablas automáticamente)
     }),
     UsersModule,
+    RolesModule,
   ],
 })
 export class AppModule {}
