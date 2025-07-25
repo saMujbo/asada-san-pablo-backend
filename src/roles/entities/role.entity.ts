ae48149ddb6abf14d84cbd571566e74c5e5dee1d
saMujbo <samuelbarrantes1@gmail.com>
2025-07-24 23:46:43 -0600

    Add-Role-entity

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,1 +1,12 @@
-export class Role {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class Role {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    RoleName: string;
+
+    @Column()
+    Description:string;
+}
