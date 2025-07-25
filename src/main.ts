686effe07d189adb88505b046ba2d0204bc90069
saMujbo <samuelbarrantes1@gmail.com>
2025-07-24 23:30:03 -0600

    SwaggerModule-configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Para configurar el Swagger 
+  const config = new DocumentBuilder()
+    .setTitle('Documentación API')
+    .setDescription('Swagger generado automáticamente con NestJS')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
