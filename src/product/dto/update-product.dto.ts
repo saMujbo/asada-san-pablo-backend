d8a4043a54a2aa0d6ef62f3fc381323267a86859
saMujbo <samuelbarrantes1@gmail.com>
2025-09-11 01:25:27 -0600

    Add-ApiProperty

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -4,12 +4,15 @@ import { IsBoolean, IsInt, IsOptional, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
+    @ApiProperty()
     @IsOptional() @IsInt() @Min(1)
     CategoryId?: number;
 
+    @ApiProperty()
     @IsOptional() @IsInt() @Min(1)
     MaterialId?: number;
 
+    @ApiProperty()
     @IsOptional() @IsInt() @Min(1)
     UnitMeasureId?: number;
 
