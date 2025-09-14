d29823fe5af1de1f33473025fae52683d377283f
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2025-09-14 15:12:46 -0600

    fix-role entity

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -2,9 +2,6 @@ import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Role {
-    static ADMIN(ADMIN: any): (target: typeof import("../../product/product.controller").ProductController) => void | typeof import("../../product/product.controller").ProductController {
-      throw new Error('Method not implemented.');
-    }
     @PrimaryGeneratedColumn()
     Id:number;
 
