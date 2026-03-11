86283bd6ea5855072caecfde3d30de09f2460fad
FranderCT <09.reelaborar-cerosa@icloud.com>
2026-03-11 17:35:50 -0600

    se añaden pruebas unitarias para categories contorller

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,15 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
+  const categoriesServiceMock = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    search: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+  const guardMock = {
+    canActivate: jest.fn().mockReturnValue(true),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
-      providers: [CategoriesService],
-    }).compile();
+      providers: [
+        {
+          provide: CategoriesService,
+          useValue: categoriesServiceMock,
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(guardMock)
+      .overrideGuard(RolesGuard)
+      .useValue(guardMock)
+      .compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
   });
