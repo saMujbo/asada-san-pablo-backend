e332d701f708f923cfe06d4b40f13ee981893051
Brenda Obando <brenobandogo0805@gmail.com>
2026-04-11 14:51:55 -0600

    validaciones en poroject projection y cambio de tipo de dato a TEXT en observacion de projectprojection

--- src/project-projection/dto/create-project-projection.dto.ts
+++ src/project-projection/dto/create-project-projection.dto.ts
@@ -1,25 +1,26 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsInt, IsOptional, IsString, ValidateNested } from "class-validator";
+import { IsInt, IsOptional, IsString, MaxLength, ValidateNested } from "class-validator";
 import { CreateProductDetailDto } from "src/product/product-detail/dto/create-product-detail.dto";
 
 
     export class CreateProjectProjectionDto {
-    @ApiProperty()
-    @IsOptional()
-    @IsString()
-    Observation?: string;
-
-    // Relación con Project (1:1)
-    @ApiProperty()
-    @IsOptional()
-    @Type(() => Number)
-    @IsInt()
-    ProjectId: number;
-
-    // Relación con ProductDetails (1:N)
-    @IsOptional()
-    @ValidateNested({ each: true })
-    @Type(() => CreateProductDetailDto)
+    @ApiProperty()                                                             
+    @IsOptional()                                                              
+    @IsString({ message: "La observación de la proyección debe ser un texto" })    
+    @MaxLength(10000, {                                                        
+      message: "La observación de la proyección no puede superar los 10000 caracteres (incluyendo formato HTML)",                                       
+    })                                                                         
+    Observation?: string;                                                      
+                                                                               
+    @ApiProperty()                                                             
+    @IsOptional()                                                              
+    @Type(() => Number)                                                        
+    @IsInt()                                                                   
+    ProjectId: number;                                                         
+                                                                               
+    @IsOptional()                                                              
+    @ValidateNested({ each: true })                                            
+    @Type(() => CreateProductDetailDto)                                        
     ProductDetails?: CreateProductDetailDto[];
     }
